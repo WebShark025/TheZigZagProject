9aaeaa0688ba201429dd9a653fdf2f70b675b191
WebShark025 <arshshia@gmail.com>
2016-09-23 18:23:46 +0330

    Update bot.py

--- bot.py
+++ bot.py
@@ -12,6 +12,6 @@ def send_welcome(message):
 
 @bot.message_handler(commands=['test', 'toast'])
 def send_test(message):
-  bot.send_message(message.chat_id, "LoL Test Msg")
+  bot.send_message(message.chat, "LoL Test Msg")
 
 bot.polling()
