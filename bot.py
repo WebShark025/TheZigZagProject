acf2c0a03781885c918843f1d7e1e24c48475dfb
WebShark025 <arshshia@gmail.com>
2016-09-23 22:05:35 +0330

    Added config. Fixed importings

--- bot.py
+++ bot.py
@@ -3,20 +3,7 @@ import telebot
 import logging
 from telebot import types
 execfile("locale.py")
-
-# CONFIG
-TOKEN = '116144035:AAHVDjt5VX-5bKGGrbtw6QJPEZF4reJcIjc' # BOT TOKEN
-DEEP_LOGGING = False # FOR DEBUGGING PURPOSES ONLY
-SAFE_ECHO = False # DOONT REPLY TO MESSAGES SENT IN SUPER GROUPS TO PREVENT SPAM
-ADMIN_ID = 98120772 # ID OF BOT'S ADMIN
-REPLIER = True # Lol.. a simple reply-to-message system xD (using dictionaries)
-
-#REPLY MESSAGES
-reply_message_list = {"salam": "slm",
-  "hi": "aleyke hi",
-  "hello": "dorood",
-}
-# END OF CONFIG
+execfile("config.py")
 
 
 logfile = open("bot.log", "a")
