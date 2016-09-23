06c2e2fb4f962b40755fc4cb781d2be799fc49bc
WebShark025 <arshshia@gmail.com>
2016-09-23 18:53:47 +0330

    Update bot.py

--- bot.py
+++ bot.py
@@ -21,7 +21,7 @@ def send_test(message):
 
 @bot.message_handler(commands=['echo'])
 def echo_message(message):
-  if len(message.text.split()) !>= 2:
+  if len(message.text.split()) < 2:
     bot.reply_to(message, "Please enter a text so I reply to it!")
   try:
     echo_msg = message.text.replace("/echo","",1)
