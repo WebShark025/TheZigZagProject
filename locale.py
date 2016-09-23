672e73b6c9fa1f97ddce2eb681c181c1cca21c22
WebShark025 <arshshia@gmail.com>
2016-09-23 21:58:33 +0330

    Created language file

---
+++ locale.py
@@ -0,0 +1,11 @@
+# LANGUAGE FILE
+
+START_MSG = "Hey, Hi!"
+TEST_MSG = "LoL Test Msg"
+SHARE_CONTACT_MSG = "Please share your contact to the bot (in a private message)."
+NO_ECHO_IN_SUPERGP_MSG = "Unfortunately I wont reply to messages sent in a supergroup to prevent spamming."
+ECHO_REPLY_MSG = "Please enter a text so I reply to it!"
+ERROR_MSG = "Error occured."
+CONTACT_RECIEVED_MSG = "New contact recieved:"
+CONTACT_FORWARDED_MSG = "Contact successfully forwarded!"
+
