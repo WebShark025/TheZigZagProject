a0988997986cee1e31e4aba10b90a25bbd9ae335
WebShark025 <arshshia@gmail.com>
2016-09-23 18:09:13 +0330

    Update bot.py

--- bot.py
+++ bot.py
@@ -2,3 +2,7 @@ import telebot
 
 TOKEN = 'BOT_TOKEN_HERE'
 bot = telebot.TeleBot(TOKEN)
+
+@bot.message_handler(commands=['start', 'help'])
+def send_welcome(message):
+  bot.reply_to(message, "Hey, Hi!")
