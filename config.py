ab523e974654c4864b4c57fce3c93149222ae49f
WebShark025 <arshshia@gmail.com>
2016-09-23 22:06:03 +0330

    Added external config for newbies.

---
+++ config.py
@@ -0,0 +1,13 @@
+# CONFIG
+TOKEN = '116144035:AAHVDjt5VX-5bKGGrbtw6QJPEZF4reJcIjc' # BOT TOKEN
+DEEP_LOGGING = False # FOR DEBUGGING PURPOSES ONLY
+SAFE_ECHO = False # DOONT REPLY TO MESSAGES SENT IN SUPER GROUPS TO PREVENT SPAM
+ADMIN_ID = 98120772 # ID OF BOT'S ADMIN
+REPLIER = True # Lol.. a simple reply-to-message system xD (using dictionaries)
+
+#REPLY MESSAGES
+reply_message_list = {"salam": "slm",
+  "hi": "aleyke hi",
+  "hello": "dorood",
+}
+# END OF CONFIG
