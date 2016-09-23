b989dc291235344e5792486c7e90149ad932620a
WebShark025 <arshshia@gmail.com>
2016-09-23 18:36:58 +0330

    Update bot.py

--- bot.py
+++ bot.py
@@ -18,8 +18,4 @@ def send_welcome(message):
 def send_test(message):
   bot.send_message(message.chat.id, "LoL Test Msg")
 
-@bot.message_handler(func=lambda msg: msg.text.encode("utf-8") == "😐")
-def poker_Detected(message):
-  bot.reply_to(message, "Poker nade koskesh")
-
 bot.polling(none_stop=True, interval=0, timeout=3)
