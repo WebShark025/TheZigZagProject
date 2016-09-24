c385991ebf589fe4a2a2b6dce583f80d55fa103e
WebShark025 <arshshia@gmail.com>
2016-09-24 07:22:45 +0330

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,8 @@
+echo "Installing..."
+git pull
+sudo pip install pyTelegramBotAPI
+chmod +x launch.sh
+
+echo "Installed successfully!"
+echo "Be sure to edit the config for your requirements!"
+echo "Exiting."
