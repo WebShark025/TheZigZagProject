1db28a5e69058cba629fd6a202c766696afa68ce
WebShark025 <arshshia@gmail.com>
2016-09-24 11:38:26 +0330

    still lol.

--- bot.py
+++ bot.py
@@ -18,7 +18,7 @@ def get_buttons_fc():
   countn = 0
   while (countn < lengthof):
     itembtnsz = types.KeyboardButton(START_BUTTONS[countn])
-    exec("itembtn" + countn + " = " + itembtnsz)
+    exec("itembtn" + str(countn) + " = " + itembtnsz)
     countn = countn + 1
   
 @bot.message_handler(commands=['start', 'help'])
