7eb7f48639fb4d3a583ed5b68907508757edee02
WebShark025 <arshshia@gmail.com>
2016-09-24 12:19:23 +0330

    now Deep supporting utf8 :/

--- bot.py
+++ bot.py
@@ -37,45 +37,45 @@ def send_welcome(message):
   markup.row(itembtn0, itembtn1, itembtn2)
   markup.row(itembtn3, itembtn4, itembtn5)
   if message.chat.type == "private":
-    bot.reply_to(message, START_MSG, reply_markup=markup)
+    bot.reply_to(message, START_MSG.encode("utf-8"), reply_markup=markup)
   else:
-    bot.reply_to(message, START_MSG)
+    bot.reply_to(message, START_MSG.encode("utf-8"))
 
 @bot.message_handler(commands=['test', 'toast'])
 def send_test(message):
-  bot.send_message(message.chat.id, TEST_MSG)
+  bot.send_message(message.chat.id, TEST_MSG.encode("utf-8"))
   
 @bot.message_handler(commands=['id'])
 def send_id(message):
   username = message.from_user.first_name.encode("utf-8")
   userid = message.from_user.id
-  reply_msg = ID_MSG
+  reply_msg = ID_MSG.encode("utf-8")
   gpid = message.chat.id
   if message.chat.type == "supergroup":
-    reply_msg = reply_msg + INGP_ID_MSG
+    reply_msg = reply_msg + INGP_ID_MSG.encode("utf-8")
   elif message.chat.type == "group":
-    reply_msg = reply_msg + INGP_ID_MSG
+    reply_msg = reply_msg + INGP_ID_MSG.encode("utf-8")
   bot.reply_to(message, reply_msg.format(username, userid, gpid), parse_mode="Markdown")
 
 @bot.message_handler(commands=['sendcontact'])
 def send_test(message):
-  bot.send_message(message.chat.id, SHARE_CONTACT_MSG)
+  bot.send_message(message.chat.id, SHARE_CONTACT_MSG.encode("utf-8"))
   
 
 @bot.message_handler(commands=['echo'])
 def echo_message(message):
   if SAFE_ECHO:
     if message.chat.type == "supergroup":
-      bot.reply_to(message, NO_ECHO_IN_SUPERGP_MSG)
+      bot.reply_to(message, NO_ECHO_IN_SUPERGP_MSG.encode("utf-8"))
       return
   if len(message.text.split()) < 2:
-    bot.reply_to(message, ECHO_REPLY_MSG, parse_mode="Markdown")
+    bot.reply_to(message, ECHO_REPLY_MSG.encode("utf-8"), parse_mode="Markdown")
     return
   try:
     echo_msg = message.text.replace("/echo","",1)
     bot.reply_to(message, echo_msg)
   except:
-    bot.send_message(messsage.chat.id, ERROR_MSG)
+    bot.send_message(messsage.chat.id, ERROR_MSG.encode("utf-8"))
   
 def message_replier(messages):
   for message in messages:
@@ -88,12 +88,12 @@ def user_greet(message):
     if message.new_chat_member.id != bot.get_me().id:
       name = message.new_chat_member.first_name
       title = message.chat.title
-      bot.send_message(message.chat.id, GP_GREETING_MSG.format(name,title), parse_mode='Markdown')
+      bot.send_message(message.chat.id, GP_GREETING_MSG.format(name,title).encode("utf-8"), parse_mode='Markdown')
   
   if message.new_chat_member.id == bot.get_me().id:
     inviter = message.from_user.id
     if inviter not in ADMINS_IDS:
-      bot.send_message(message.chat.id, NON_ADMIN_ADDED_BOT_MSG)
+      bot.send_message(message.chat.id, NON_ADMIN_ADDED_BOT_MSG.encode("utf-8"))
       bot.leave_chat(message.chat.id)
     else:
       bot.send_message(message.chat.id, BOT_JOINED_MSG)
@@ -103,14 +103,14 @@ def user_greet(message):
   if GP_FAREWELL:
     name = message.left_chat_member.first_name
     title = message.chat.title
-    bot.send_message(message.chat.id, GP_FAREWELL_MSG.format(name,title), parse_mode='Markdown')
+    bot.send_message(message.chat.id, GP_FAREWELL_MSG.format(name,title).encode("utf-8"), parse_mode='Markdown')
   
 @bot.message_handler(func=lambda m: True, content_types=['contact'])
 def contact_forwarder(contact):
   if contact.chat.type == "private":
-    bot.send_message(SUDO_ID, CONTACT_RECIEVED_MSG)
+    bot.send_message(SUDO_ID, CONTACT_RECIEVED_MSG.encode("utf-8"))
     bot.forward_message(SUDO_ID, contact.chat.id, contact.message_id)
-    bot.reply_to(contact, CONTACT_FORWARDED_MSG)
+    bot.reply_to(contact, CONTACT_FORWARDED_MSG.encode("utf-8"))
 
 logger = telebot.logger
 if DEEP_LOGGING:
