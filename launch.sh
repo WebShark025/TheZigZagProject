b23abcbefc1dc033380d2ca512ea88cd8fcad8b1
root <root@webshark25.sdco>
2016-09-25 14:04:43 -0400

    Fixed the autolaunch (its still dangerous and NOT NEEDED for PYTHON BOTS)

--- launch.sh
+++ launch.sh
@@ -1,7 +1,9 @@
-read -p "Do you want to use auto launch? (to kill it=Ctrl+C) " -n 1 -r
+read -p "(SO DANGEROUS) Do you want to use auto launch? (to kill it=HOLD Ctrl+C) " -n 1 -r
+echo " "
 if [[ $REPLY =~ ^[Yy]$ ]]
 then
   while true
+  do
     python bot.py
     echo "Bot has crashed! Launching it again."
   done
