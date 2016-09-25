b3a28ace0a54c4f05a66f5f70e62760c73342bca
WebShark025 <arshshia@gmail.com>
2016-09-25 21:27:44 +0330

    Added autolaunch.

--- launch.sh
+++ launch.sh
@@ -1,1 +1,9 @@
-python bot.py
+read -p "Are you sure? " -n 1 -r
+if [[ $REPLY =~ ^[Yy]$ ]]
+then
+  while true
+  python bot.py
+  echo "Bot has crashed! Launching it again."
+else
+  python bot.py
+fi
