cea461487d1715b89283ecd391f0018f788cddc9
WebShark025 <arshshia@gmail.com>
2016-09-25 18:19:31 +0330

    added /bc & finalized /members

--- bot.py
+++ bot.py
@@ -86,7 +86,10 @@ def send_members(message):
     
 @bot.message_handler(commands=['bc'])
 def bc_msg(message):
-  if message.from_user.id in ADMIN_IDS:
+  if message.from_user.id in ADMINS_IDS:
+    if len(message.text.split()) < 2:
+      bot.reply_to(message, "What should I broadcast?")
+      return
     bcmsg = message.text.replace("/bc ","")
     allmembers = list(redisserver.smembers('zigzag_members'))
     for userid in allmembers:
