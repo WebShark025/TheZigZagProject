a34e46a45f4b187bf2e67509f83269d717c3fac6
WebShark025 <arshshia@gmail.com>
2016-09-28 17:29:12 +0330

    YEAH IT WORKED

--- plugins/messenger.py
+++ plugins/messenger.py
@@ -8,7 +8,7 @@ def support(message):
   if message.from_user.id not in in_chat_with_support:
     bot.reply_to(message, JOINED_MESSENGER_MSG, parse_mode="HTML")
     in_chat_with_support.append(message.from_user.id)
-    bot.send_messagae("-" + str(SUPPORT_GP), "User " + str(message.from_user.id) + " - " + str(message.from_user.first_name) + str(message.from_user.first_name) + " joined the support chat!") 
+    bot.send_message("-" + str(SUPPORT_GP), "User " + str(message.from_user.id) + " - " + str(message.from_user.first_name) + str(message.from_user.first_name) + " joined the support chat!") 
   else:
     bot.reply_to(message, ALREADY_IN_MESSENGER_MSG, parse_mode="HTML")
 @bot.message_handler(commands=['leave'])
@@ -20,7 +20,7 @@ def leave_support(message):
   if message.from_user.id in in_chat_with_support:
     bot.reply_to(message, LEFT_MESSENGER_MSG, parse_mode="HTML")
     in_chat_with_support.remove(message.from_user.id)
-    bot.send_messagae("-" + str(SUPPORT_GP), "User " + str(message.from_user.id) + " - " + str(message.from_user.first_name) + str(message.from_user.first_name) + " left the support chat.") 
+    bot.send_message("-" + str(SUPPORT_GP), "User " + str(message.from_user.id) + " - " + str(message.from_user.first_name) + str(message.from_user.first_name) + " left the support chat.") 
   else:
     bot.reply_to(message, NOT_IN_MESSENGER_MSG, parse_mode="HTML")
 @bot.message_handler(commands=['force_user_leave'])
