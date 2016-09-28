546fecda3758950ff31bd654719064da171cb69f
WebShark025 <arshshia@gmail.com>
2016-09-28 22:05:33 +0330

    Update callback_data.py

--- plugins/callback_data.py
+++ plugins/callback_data.py
@@ -1,1 +1,5 @@
-.
+@bot.callback_query_handler(func=lambda call: True)
+def callback_inline(call):
+  if call.message:
+    if call.data == "help":
+      bot.send_message(call.from_user.id, START_MSG.encode("utf-8"), parse_mode="Markdown")
