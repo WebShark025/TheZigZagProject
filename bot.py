9e534545575ca1a0e3e92685b751c67eb7fa7130
WebShark025 <arshshia@gmail.com>
2016-09-28 17:00:51 +0330

    Update bot.py

--- bot.py
+++ bot.py
@@ -42,24 +42,6 @@ logfile.write("Bot Started: " + str(time) + ". Enabled plugins:" + str(enabled_p
 print("Bot started: " + str(time))
 messanger_list = []
 contacter_list = []
-def message_replier(messages):
-  for message in messages:
-    userid = message.from_user.id
-    banlist = redisserver.sismember('zigzag_banlist', '{}'.format(userid))
-    if banlist:
-      return
-    if userid in messanger_list:
-      bot.reply_to(message, MESSANGER_LEAVE_MSG, parse_mode="Markdown")
-      messanger_list.remove(userid)
-      bot.forward_message("-" + str(SUPPORT_GP), message.chat.id, message.message_id)
-      return
-    if REPLIER:
-      if message.text in reply_message_list:
-        bot.reply_to(message, reply_message_list.get(message.text), parse_mode="Markdown")
-    if message.text == "Send feedback":
-      bot.reply_to(message, MESSANGER_JOIN_MSG, parse_mode="Markdown")
-      messanger_list.append(userid)
-      return
 logger = telebot.logger
 if DEEP_LOGGING:
   print("Debugging enabled.")
