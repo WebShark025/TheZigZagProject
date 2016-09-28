6996d7cc41ed1a80a8de759837c5b911bf8f354c
WebShark025 <arshshia@gmail.com>
2016-09-28 17:26:19 +0330

    I hope it works

--- plugins/message_handler.py
+++ plugins/message_handler.py
@@ -7,6 +7,7 @@ def message_replier(messages):
     if userid in messanger_list:
       bot.reply_to(message, MESSANGER_LEAVE_MSG, parse_mode="Markdown")
       messanger_list.remove(userid)
+      bot.send_message("-" + str(SUPPORT_GP), "New feedback!:")
       bot.forward_message("-" + str(SUPPORT_GP), message.chat.id, message.message_id)
       return
     if REPLIER:
@@ -16,3 +17,13 @@ def message_replier(messages):
       bot.reply_to(message, MESSANGER_JOIN_MSG, parse_mode="Markdown")
       messanger_list.append(userid)
       return
+    if userid in in_chat_with_support:
+      bot.forward_message("-" + str(SUPPORT_GP), message.chat.id, message.message_id)
+      return
+    if message.from_user.id in ADMINS_IDS:
+      if message.chat.id == -SUPPORT_GP:
+        try:
+          bot.forward_message(message.reply_to_message.forward_from.id, message.chat.id, message.message_id)
+          bot.reply_to(message, "REPLY SENT")
+        except:
+          bot.reply_to(message, "ERROR SENDING?")
