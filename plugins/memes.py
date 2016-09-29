24a1d9cab44efc75cd8c0397cd031fec09febc73
WebShark025 <arshshia@gmail.com>
2016-09-29 10:34:53 +0330

    Update memes.py

--- plugins/memes.py
+++ plugins/memes.py
@@ -1,3 +1,8 @@
 @bot.message_handler(commands=['meme'])
-def echo_message(message):
-  
+def meme_image(message):
+  if len(message.text.split("||")) < 3:
+    bot.reply_to(message, MEME_NEA_MSG, parse_mode="Markdown")
+    return
+  args = message.text.split("||")
+  urllib.urlretrieve("http://apimeme.com/meme?meme={}&top={}&bottom={}".format(args[0], args[1], args[2]), 'meme.jpg')
+  bot.send_photo(message.chat.id, open('meme.jpg'), caption=" By the @TheZigZagBot")
