15666211c0ff39a9acb66976e514b62b1111c701
WebShark025 <arshshia@gmail.com>
2016-09-29 10:17:08 +0330

    Fixed a little bug

--- plugins/callback_data.py
+++ plugins/callback_data.py
@@ -3,6 +3,7 @@ def callback_inline(call):
   if call.message:
     if call.data == "help":
       bot.send_message(call.message.chat.id, START_MSG.encode("utf-8"), parse_mode="Markdown")
+      bot.answer_callback_query(callback_query_id=call.id, show_alert=False, text="Here you are!")
     if call.data == "showit":
       markup = types.ReplyKeyboardMarkup()
       numbers = list(range(3, 3000, 3))
@@ -23,3 +24,5 @@ def callback_inline(call):
       except:
         lolalola = 0
       bot.send_message(call.message.chat.id, SHOWED_BUTTONS_MSG, reply_markup=markup, parse_mode="Markdown")
+      bot.answer_callback_query(callback_query_id=call.id, show_alert=False, text="Here you are!")
+
