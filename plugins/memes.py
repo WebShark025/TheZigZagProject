6af5cf2a9f899221b603d70bb7796976ee8b0a83
WebShark025 <arshshia@gmail.com>
2016-09-29 10:46:41 +0330

    Added /meme command!

--- plugins/memes.py
+++ plugins/memes.py
@@ -3,6 +3,8 @@ def meme_image(message):
   if len(message.text.split("||")) < 3:
     bot.reply_to(message, MEME_NEA_MSG, parse_mode="Markdown")
     return
-  args = message.text.split("||")
+  args = message.text.replace("/meme ","").replace(" ", "%20").split("||")
+  bot.reply_to(message, "Processing..")
   urllib.urlretrieve("http://apimeme.com/meme?meme={}&top={}&bottom={}".format(args[0], args[1], args[2]), 'meme.jpg')
+#  print("http://apimeme.com/meme?meme={}&top={}&bottom={}".format(args[0], args[1], args[2]))
   bot.send_photo(message.chat.id, open('meme.jpg'), caption=" By the @TheZigZagBot")
