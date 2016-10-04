fda13fe841776708ae2e3cbc3ca20851dbc57878
WebShark025 <arshshia@gmail.com>
2016-10-04 17:02:33 +0330

    Update members.py

--- plugins/members.py
+++ plugins/members.py
@@ -1,7 +1,7 @@
 @bot.message_handler(commands=['members'])
 def send_members(message):
   if message.from_user.id in ADMINS_IDS:
-    bot.send_message(message.chat.id, "All members: " + str(redisserver.scard('zigzag_members')) + " \nBanned members: " + str(redisserver.scard('zigzag_banlist')), parse_mode="Markdown")
+    bot.send_message(message.chat.id, "All members: " + str(redisserver.scard('zigzag_members')) + " \nBlocked users: " + str(redisserver.scard('zigzag_blocked')) + " \nBanned members: " + str(redisserver.scard('zigzag_banlist')), parse_mode="Markdown")
 #    allmembers = list(redisserver.smembers('zigzag_members'))
 #    bot.send_message(message.chat.id, "First member: " + str(allmembers[0]), parse_mode="Markdown")
   else:
