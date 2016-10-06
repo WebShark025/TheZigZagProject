f2babab4926818e41d3893120c26a60385e1d094
WebShark025 <arshshia@gmail.com>
2016-10-06 19:49:48 +0330

    Fixed some bugs

--- plugins/broadcast.py
+++ plugins/broadcast.py
@@ -7,11 +7,16 @@ def bc_msg(message):
     bcmsg = message.text.replace("/bc ","")
     allmembers = list(redisserver.smembers('zigzag_members'))
     for userid in allmembers:
+      rc = 0
       try:
         bot.send_message(userid, bcmsg, parse_mode="HTML")
+        rc = rc + 1
+        blocklist = redisserver.sismember('zigzag_blocked', '{}'.format(userid))
+        if blocklist:
+          redisserver.srem('zigzag_blocked', userid)
       except:
         redisserver.sadd('zigzag_blocked',userid)
-    bot.reply_to(message, "Successfully broadcasted!")
+    bot.reply_to(message, "Successfully broadcasted to " + str(rc) + " users!")
   else:
     bot.send_message(message.chat.id, "You dont have permission.")
 
