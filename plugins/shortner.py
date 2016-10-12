cafdf6b3d3fb3fbba6bd5166450695c317e527d5
Arshia shakeri <arshshia@gmail.com>
2016-10-12 09:33:26 +0330

    Update shortner.py

--- plugins/shortner.py
+++ plugins/shortner.py
@@ -4,9 +4,9 @@ def shortit(m):
   banlist = redisserver.sismember('zigzag_banlist', '{}'.format(userid))
   if banlist:
     return
-  if len(m.text.replace("Link ", "", 1).split()) < 2:
+  if len(m.text.replace("🔗 Link shortner", "", 1).split()) < 2:
     bot.reply_to(m, SHORTNER_NEA_MSG, parse_mode="Markdown")
     return
-  text = m.text.replace("/short ","", 1).replace("Link shortner", "", 1)
+  text = m.text.replace("/short ","", 1).replace("🔗 Link shortner", "", 1)
   res = urllib.urlopen("http://r1z.ir/api.php?long={}".format(text).replace("+","%2B")).read()
   bot.send_message(m.chat.id, "Shorten link: `{}`".format(res), parse_mode="Markdown", disable_web_page_preview=True)
