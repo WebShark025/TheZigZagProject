7330d2253c89400a5603fe21f2be00c4e062b68d
Arshia shakeri <arshshia@gmail.com>
2016-10-12 21:58:24 +0330

    Update inline.py

--- plugins/inline.py
+++ plugins/inline.py
@@ -12,16 +12,17 @@ def default_query(inline_query):
     print(e)
 @bot.inline_handler(lambda query: query.query.split()[0] == 'echo')
 def query_text(inline_query):
+  userlang = redisserver.get("settings:user:language:" + str(inline_query.from_user.id))
   try:
     if inline_query.query == "echo":
-      r = types.InlineQueryResultArticle('1', 'Please enter a text!', types.InputTextMessageContent('I Love empty texts.'))
+      r = types.InlineQueryResultArticle('1', language[userlang]["INLINE_ENTERTEXT_MSG"], types.InputTextMessageContent('I Love empty texts.'))
       bot.answer_inline_query(inline_query.id, [r])
     if len(inline_query.query.split()) > 1:
       try:
-        r3 = types.InlineQueryResultArticle('3', 'Echo your message using HTML parse mode ;)', types.InputTextMessageContent(inline_query.query.replace("echo ", "", 1), parse_mode="HTML"))
+        r3 = types.InlineQueryResultArticle('3', language[userlang]["INLINE_SUCCESSECHO_MSG"], types.InputTextMessageContent(inline_query.query.replace("echo ", "", 1), parse_mode="HTML"))
         bot.answer_inline_query(inline_query.id, [r3], cache_time=1, is_personal=True)
       except:
-        r3 = types.InlineQueryResultArticle('3', 'Error occured. One of your tags arent closed!', types.InputTextMessageContent("I forgot to close a tag."))
+        r3 = types.InlineQueryResultArticle('3', language[userlang]["INLINE_ERRORECHO_MSG"], types.InputTextMessageContent("I forgot to close a tag."))
         bot.answer_inline_query(inline_query.id, [r3], cache_time=1, is_personal=True)
   except Exception as e:
     print(e)
@@ -29,9 +30,10 @@ def query_text(inline_query):
 
 @bot.inline_handler(lambda query: query.query.split()[0] == 'calc')
 def query_text(inline_query):
+  userlang = redisserver.get("settings:user:language:" + str(inline_query.from_user.id))
   try:
     if inline_query.query == "calc":
-      r = types.InlineQueryResultArticle('1', 'Please enter what you need I calculate.', types.InputTextMessageContent('0! :D'))
+      r = types.InlineQueryResultArticle('1', language[userlang]["INLINE_CALC_MSG"], types.InputTextMessageContent('0! :D'))
       bot.answer_inline_query(inline_query.id, [r])
     if len(inline_query.query.split()) > 1:
       text = inline_query.query.replace("calc ", "",1)
@@ -47,9 +49,10 @@ def query_text(inline_query):
 
 @bot.inline_handler(lambda query: query.query.split()[0] == 'hideit')
 def hideit_text(inline_query):
+  userlang = redisserver.get("settings:user:language:" + str(inline_query.from_user.id))
   try:
     if inline_query.query == "hideit":
-      r = types.InlineQueryResultArticle('1', 'Please enter a text so I can hide it!', types.InputTextMessageContent('I Love empty texts.'))
+      r = types.InlineQueryResultArticle('1', language[userlang]["INLINE_HIDEIT_MSG"], types.InputTextMessageContent('I Love empty texts.'))
       bot.answer_inline_query(inline_query.id, [r])
     if len(inline_query.query.split()) > 1:
       try:
@@ -58,7 +61,7 @@ def hideit_text(inline_query):
         markupz.add(markupid)
         query_text = {inline_query.id: inline_query.query.replace("hideit ", "", 1)}
         querymessages.update(query_text)
-        r3 = types.InlineQueryResultArticle('3', 'Send hidden text!', types.InputTextMessageContent("❓❓❓"), reply_markup=markupz)
+        r3 = types.InlineQueryResultArticle('3', 'Send!', types.InputTextMessageContent("❓❓❓"), reply_markup=markupz)
         bot.answer_inline_query(inline_query.id, [r3], cache_time=1, is_personal=True)
       except:
         r3 = types.InlineQueryResultArticle('3', 'Error occured.', types.InputTextMessageContent("Unexpected error occured."))
@@ -69,9 +72,10 @@ def hideit_text(inline_query):
 
 @bot.inline_handler(lambda query: query.query.split()[0] == 'weather')
 def query_text(inline_query):
+  userlang = redisserver.get("settings:user:language:" + str(inline_query.from_user.id))
   try:
     if inline_query.query == "weather":
-      r = types.InlineQueryResultArticle('1', 'Please enter a city!', types.InputTextMessageContent('Wait, what?'))
+      r = types.InlineQueryResultArticle('1', language[userlang]["INLINE_WEATHER_MSG"], types.InputTextMessageContent('Wait, what?'))
       bot.answer_inline_query(inline_query.id, [r])
     if len(inline_query.query.split()) > 1:
       city = inline_query.query.replace("weather ","").replace(" ", "%20")
@@ -81,7 +85,7 @@ def query_text(inline_query):
         print("[WeatherInline] Exception occured")
         return
       try:
-        r3 = types.InlineQueryResultArticle('3', 'Send current weather of ' + str(url["name"]), types.InputTextMessageContent("💢 Current status of *" + str(url["name"]) + "*: \n\n🌍 Country: `" + str(url["sys"]["country"]) + "` \n☀️ Temperature: `" + str(url["main"]["temp"]) + "°C` \n" + "🌤 Weather: `" + str(url["weather"][0]["main"]) + "` \n💨 Wind: `" + str(url["wind"]["speed"]) + "m/s` \n💧 Humidity: `" + str(url["main"]["humidity"]) + "%`", parse_mode="Markdown"))
+        r3 = types.InlineQueryResultArticle('3', language[userlang]["INLINE_SENDWEATHER_MSG"] + str(url["name"]), types.InputTextMessageContent("💢 Current status of *" + str(url["name"]) + "*: \n\n🌍 Country: `" + str(url["sys"]["country"]) + "` \n☀️ Temperature: `" + str(url["main"]["temp"]) + "°C` \n" + "🌤 Weather: `" + str(url["weather"][0]["main"]) + "` \n💨 Wind: `" + str(url["wind"]["speed"]) + "m/s` \n💧 Humidity: `" + str(url["main"]["humidity"]) + "%`", parse_mode="Markdown"))
         bot.answer_inline_query(inline_query.id, [r3], cache_time=1, is_personal=True)
       except:
         r3 = types.InlineQueryResultArticle('3', 'Error occured.', types.InputTextMessageContent("Unexpected error occured."))
@@ -92,9 +96,10 @@ def query_text(inline_query):
 
 @bot.inline_handler(lambda query: query.query.split()[0] == 'time')
 def query_text(inline_query):
+  userlang = redisserver.get("settings:user:language:" + str(inline_query.from_user.id))
   try:
     if inline_query.query == "time":
-      r = types.InlineQueryResultArticle('1', 'Please enter timezone/city/region/etc!', types.InputTextMessageContent('Time in nowhere: 00:00:00'))
+      r = types.InlineQueryResultArticle('1', language[userlang]["INLINE_SENDTIME_MSG"], types.InputTextMessageContent('Time in nowhere: 00:00:00'))
       bot.answer_inline_query(inline_query.id, [r])
     if len(inline_query.query.split()) > 1:
       city = inline_query.query.split()[1]
@@ -115,7 +120,7 @@ def query_text(inline_query):
         return
       time = json.load(urllib.urlopen("https://script.google.com/macros/s/AKfycbyd5AcbAnWi2Yn0xhFRbyzS4qMq1VucMVgVvhul5XqS9HkAyJY/exec?tz={}".format(timezone)))
       try:
-        r3 = types.InlineQueryResultArticle('3', 'Send current time in ' + str(timezone), types.InputTextMessageContent("Current time in *" + timezone + "*: \n" + time["fulldate"], parse_mode="Markdown"))
+        r3 = types.InlineQueryResultArticle('3', language[userlang]["INLINE_TIMETIME_MSG"] + str(timezone), types.InputTextMessageContent("Current time in *" + timezone + "*: \n" + time["fulldate"], parse_mode="Markdown"))
         bot.answer_inline_query(inline_query.id, [r3], cache_time=1, is_personal=True)
       except:
         r3 = types.InlineQueryResultArticle('3', 'Error occured.', types.InputTextMessageContent("Unexpected error occured."))
