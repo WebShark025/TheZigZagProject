4e7c0162a446211305163f339d46f39bf25e2df1
Arshia shakeri <arshshia@gmail.com>
2016-10-12 08:44:55 +0330

    Update messenger.py

--- plugins/messenger.py
+++ plugins/messenger.py
@@ -1,4 +1,6 @@
 in_chat_with_support = []
+waiting_support_approval = []
+
 @bot.message_handler(commands=['support'])
 def support(message):
   userid = message.from_user.id
@@ -6,9 +8,12 @@ def support(message):
   if banlist:
     return
   if message.from_user.id not in in_chat_with_support:
-    bot.reply_to(message, JOINED_MESSENGER_MSG, parse_mode="HTML")
-    in_chat_with_support.append(message.from_user.id)
-    bot.send_message("-" + str(SUPPORT_GP), "User " + str(message.from_user.id) + " - " + str(message.from_user.first_name) + str(message.from_user.first_name) + " joined the support chat!") 
+    if userid not in waiting_support_approval:
+      bot.reply_to(message, WAITING_APPROVAL_MESSENGER_MSG, parse_mode="HTML")
+      waiting_support_approval.append(message.from_user.id)
+      bot.send_message("-" + str(SUPPORT_GP), "User " + str(message.from_user.id) + " - " + str(message.from_user.first_name) + str(message.from_user.first_name) + " joined the support chat!") 
+    else:
+      bot.reply_to(message, "Please wait. Your chat request hasnt been still manually accepted!")
   else:
     bot.reply_to(message, ALREADY_IN_MESSENGER_MSG, parse_mode="HTML")
 @bot.message_handler(commands=['leave'])
@@ -36,6 +41,37 @@ def forceleave(message):
       in_chat_with_support.remove(userid)
     else:
       bot.reply_to(message, "User not in chat.")
+  else:
+    bot.send_message(message.chat.id, "You dont have permission.")
+@bot.message_handler(commands=['accept_chat'])
+def acceptchat(message):
+  if message.from_user.id in ADMINS_IDS:
+    if len(message.text.split()) < 2:
+      bot.reply_to(message, "Dude, enter an ID.", parse_mode="Markdown")
+      return
+    userid = int(message.text.split()[1])
+    if userid in waiting_support_approval:
+      bot.reply_to(message, "Accepted chat request", parse_mode="HTML")
+      bot.send_message(userid, ACCEPTED_MESSENGER_MSG)
+      waiting_support_approval.remove(userid)
+      in_chat_with_support.append(userid)
+    else:
+      bot.reply_to(message, "User not in approval list")
+  else:
+    bot.send_message(message.chat.id, "You dont have permission.")
+@bot.message_handler(commands=['deny_chat'])
+def acceptchat(message):
+  if message.from_user.id in ADMINS_IDS:
+    if len(message.text.split()) < 2:
+      bot.reply_to(message, "Dude, enter an ID.", parse_mode="Markdown")
+      return
+    userid = int(message.text.split()[1])
+    if userid in waiting_support_approval:
+      bot.reply_to(message, "Denied chat request", parse_mode="HTML")
+      bot.send_message(userid, DENIED_MESSENGER_MSG)
+      waiting_support_approval.remove(userid)
+    else:
+      bot.reply_to(message, "User not in approval list")
   else:
     bot.send_message(message.chat.id, "You dont have permission.")
 # MESSAGE HANDLING SYSTEM IN MESSAGE_HANDLER PLUGIN
