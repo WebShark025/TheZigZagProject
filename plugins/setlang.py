bcbd460122fc62940892959a76a899e5954b51b1
WebShark025 <arshshia@gmail.com>
2016-10-13 11:12:27 +0330

    /Setlang

--- plugins/setlang.py
+++ plugins/setlang.py
@@ -10,13 +10,13 @@ def setlang_message(message):
   if len(message.text.split()) < 2:
     bot.reply_to(message, "Please choose a language! En/Fa \nلطفا زبان مورد نظر خود را وارد کنید! فارسی/انگلیسی", parse_mode="Markdown")
     return
-  try:
-    langz = message.text.replace("/setlang","",1).replace("/Setlang","",1)
+  if True:
+    langz = message.text.replace("/setlang ","",1).replace("/Setlang ","",1)
     if langz == "en":
-      redisserver.set("settings:user:language:" + str(call.from_user.id), "en")
+      redisserver.set("settings:user:language:" + str(message.from_user.id), "en")
       bot.send_message(message.chat.id, "Success!")
     elif langz == "fa":
-      redisserver.set("settings:user:language:" + str(call.from_user.id), "fa")
+      redisserver.set("settings:user:language:" + str(message.from_user.id), "fa")
       bot.send_message(message.chat.id, "انجام شد!")
-  except:
-    pass
+    else:
+      bot.reply_to(message, "Language not found \n زبان یافت نشد")
