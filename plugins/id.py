223e2fd88ce66e9cf6b748e154b97dee6c2c5a9d
WebShark025 <arshshia@gmail.com>
2016-10-15 18:06:53 +0330

    Little change to /id

--- plugins/id.py
+++ plugins/id.py
@@ -13,15 +13,9 @@ def send_id(message):
     reply_msg = reply_msg + language[userlang]["INGP_ID_MSG"]
   elif message.chat.type == "group":
     reply_msg = reply_msg + language[userlang]["INGP_ID_MSG"]
-  if message.forward_from:
-    reply_msg = reply_msg + language[userlang]["FORWARDED_ID_MSG"].format(message.forward_from.id)
-  if message.reply_to_message && message.reply_to_message.forward_from:
-    reply_msg = reply_msg + language[userlang]["FORWARDED_ID_MSG"].format(message.reply_to_message.forward_from.id)
-  try:
+  if message.reply_to_message:
     repliedid = message.reply_to_message.from_user.id
-    reply_msg = reply_msg + language[userlang]["REPLIED_ID_MSG"]
-    bot.reply_to(message, reply_msg.format(username, userid, gpid, repliedid), parse_mode="Markdown")
-    return
-  except:
-    adaadaadadadadadadada = "00"
-    bot.reply_to(message, reply_msg.format(username, userid, gpid), parse_mode="Markdown")
+    reply_msg = reply_msg + language[userlang]["REPLIED_ID_MSG"].format(repliedid)
+    if message.reply_to_message.forward_from:
+      reply_msg = reply_msg + language[userlang]["FORWARDED_ID_MSG"].format(message.reply_to_message.forward_from.id)
+  bot.reply_to(message, reply_msg.format(username, userid, gpid), parse_mode="Markdown")
