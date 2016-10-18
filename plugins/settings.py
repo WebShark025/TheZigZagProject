4c1275c340d2749b2d69b45c8c4cacdc6112ec60
Arshia shakeri <arshshia@gmail.com>
2016-10-18 19:26:32 +0330

    Update settings.py

--- plugins/settings.py
+++ plugins/settings.py
@@ -1,1 +1,13 @@
-.
+# -*- coding: utf-8 -*-
+
+@bot.message_handler(commands=['settings', 'Settings'])
+def send_settings(message):
+  userlang = redisserver.get("settings:user:language:" + str(message.from_user.id))
+  userid = message.from_user.id
+  banlist = redisserver.sismember('zigzag_banlist', '{}'.format(userid))
+  if banlist:
+    return
+  markup = types.InlineKeyboardMarkup()
+  markupib = types.InlineKeyboardButton("Show settings!", callback_data='settings')
+  markup.add(markupib
+  bot.send_message(call.message.chat.id, "Please, press on the button below :)", parse_mode="Markdown", reply_markup=markup)
